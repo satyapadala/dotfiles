601fa8a81ba48ce1407ea2e2a0b235e74110ea3c
Satya Padala <satya.eee14@gmail.com>
2024-01-09 09:47:43 +0530

    Update packer.lua

--- nvim/lua/satya/packer.lua
+++ nvim/lua/satya/packer.lua
@@ -4,56 +4,81 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
 	-- telescope
-	use({
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
-		-- or                            , branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	})
-
-	-- rose pine color scheme
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
-	})
-	use(
-		'nvim-treesitter/nvim-treesitter',
-		{ run = ':TSUpdate' }
-	)
-	use('nvim-treesitter/playground')
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-
 	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			--- Uncomment these if you want to manage LSP servers from neovim
-			{ 'williamboman/mason.nvim' },
-			{ 'williamboman/mason-lspconfig.nvim' },
+		use({
+			"nvim-telescope/telescope.nvim",
+			requires = {
+				{ "nvim-telescope/telescope-live-grep-args.nvim" },
+				{ "nvim-lua/plenary.nvim" },
+			},
+			config = function()
+				require("telescope").load_extension("live_grep_args")
+			end
+		}),
 
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'L3MON4D3/LuaSnip' },
-		}
+		-- rose pine color scheme
+		use({
+			'rose-pine/neovim',
+			as = 'rose-pine',
+			config = function()
+				vim.cmd('colorscheme rose-pine')
+			end
+		}),
+		use(
+			'nvim-treesitter/nvim-treesitter',
+			{ run = ':TSUpdate' }
+		),
+		use('nvim-treesitter/playground'),
+		use('theprimeagen/harpoon'),
+		use('mbbill/undotree'),
+		use('tpope/vim-fugitive'),
+
+		use {
+			'VonHeikemen/lsp-zero.nvim',
+			branch = 'v3.x',
+			requires = {
+				--- Uncomment these if you want to manage LSP servers from neovim
+				{ 'williamboman/mason.nvim' },
+				{ 'williamboman/mason-lspconfig.nvim' },
+
+				-- LSP Support
+				{ 'neovim/nvim-lspconfig' },
+				-- Autocompletion
+				{ 'hrsh7th/nvim-cmp' },
+				{ 'hrsh7th/cmp-nvim-lsp' },
+				{ 'L3MON4D3/LuaSnip' },
+			}
+		},
+		use("folke/zen-mode.nvim"),
+		use("github/copilot.vim"),
+		use("eandrju/cellular-automaton.nvim"),
+		use("laytan/cloak.nvim"),
+		use("mfussenegger/nvim-jdtls"),
+		-- Obsidian
+		use({
+			"epwalsh/obsidian.nvim",
+			tag = "*", -- recommended, use latest release instead of latest commit
+			requires = {
+				-- Required.
+				"nvim-lua/plenary.nvim",
+
+				-- see below for full list of optional dependencies 👇
+			},
+			config = function()
+				require("obsidian").setup({
+					workspaces = {
+						{
+							name = "personal",
+							path =
+							"/Users/satyap/iCloud Drive (Archive)/Documents/Uber/Obsidian/MyPersonalVault",
+						},
+					},
+
+					-- see below for full list of options 👇
+				})
+			end,
+		}),
+		use("ojroques/vim-oscyank"),
 	}
-	use("folke/zen-mode.nvim")
-	use("github/copilot.vim")
-	use("eandrju/cellular-automaton.nvim")
-	use("laytan/cloak.nvim")
-	use(
-		'fatih/vim-go',
-		{ run = ':GoInstallBinaries' })
-	use("preservim/nerdcommenter")
-	use 'mfussenegger/nvim-dap'
-	use 'leoluz/nvim-dap-go'
-	use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
 end
 )
